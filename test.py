4384224a152d8432cdb5278debd430f2fe812722
Wang <taptube@gmail.com>
2015-09-13 04:49:23 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-# coding=utf8
-__author__ = 'Wang<taptube@gmail.com>'
-
-import requests,traceback
-
-try:
-    i = requests.get('http://www.121888.com',timeout =0.001)
-except:
-    print traceback.print_exc()
